9e5c03f837931c1a4d193a58a80e05aa3cbf38a7
Sagarraj08 <sagarraj8294@gmail.com>
2023-03-07 12:12:45 +0530

    s

---
+++ 4/3/loginsystem.py
@@ -0,0 +1,25 @@
+class places:
+    def __init__(self):
+        print("Places to must visit")
+        print("1.Ahmedabad 2.Agra 3.Delhi 4.Banglore 5.Mumbai 6.Kolkata 7.Lonavala 8.Peris 9.Dubai 10.Switzerland")
+
+class register:
+    def __init__(self,x):
+        self.x=x
+    def reg():
+        x=input("Do you want to Register?[y/n]")
+        if x=="y" or x=="Y":
+            print("Enter registration details")
+        elif x=="n" or x=="N":
+            print("THANK YOU")
+class details:
+    def __init__(dt,name,uname,pswd):
+        dt.name=name
+        dt.uname=uname
+        dt.pswd=pswd
+    def dtls():
+        print("Enter username")
+        
+
+    
+    
